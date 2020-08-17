508184db1fe9ce02220163321c835b80d54f1d04
Khadija <khadijahasta@gmail.com>
2020-08-17 21:02:35 +0530

    Commit from est guy

---
+++ SD/src/hd.java
@@ -0,0 +1,8 @@
+
+public class hd {
+
+	
+	public static void main(String[] args) {
+		System.out.println("ijdk");
+	}
+}
--- SD/src/hi.java
+++ SD/src/hi.java
@@ -4,6 +4,10 @@ public class hi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hdjd");
+System.out.println("kkfj");
+System.out.println("yuweyuew");
+System.out.println("mcxmc");
+
 	}
 
 }
